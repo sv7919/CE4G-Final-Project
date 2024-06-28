1de5c64a918871ca28b93aa7001d67ac2e315d12
sv7919 <vijayansiddarth@gmail.com>
2024-06-28 09:17:46 -0400

    add: sensor initialization and main code

--- include/config.hpp
+++ include/config.hpp
@@ -1,1 +1,16 @@
 //This branch is solely for the air sensor component
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME680 bme; // I2C
+//Adafruit_BME680 bme(BME_CS); // hardware SPI
+//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
