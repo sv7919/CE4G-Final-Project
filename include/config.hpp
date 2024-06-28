10ea7bd62a35d8741ef286f687bf430f543cc683
sv7919 <vijayansiddarth@gmail.com>
2024-06-28 09:32:24 -0400

    add: config created and structure of main code

--- include/config.hpp
+++ include/config.hpp
@@ -1,1 +1,18 @@
 //This branch is solely for dashboard related things connected to Adafruit.IO
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+// Fetch your key from Adafruit
+#define IO_USERNAME "your username here"
+#define IO_KEY "your key here"
+
+/******************************* WIFI **************************************/
+
+#define WIFI_SSID "your SSID here"
+#define WIFI_PASS "your password here"
+
+#include <AdafruitIO_WiFi.h>
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+#endif  
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
 #include <Arduino.h>
+#include <config.hpp>
+
+void handleMessage(AdafruitIO_Data *data);
+
+AdafruitIO_Feed *location1 = io.feed("Fill in");
+AdafruitIO_Feed *location2 = io.feed("Fill in");
 
 void setup() {
+  Serial.begin(115200);
 
+  // wait for serial monitor to open
+  while (!Serial);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // set up a message handler for the 'button_f' feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+
+  /**
+   * @attention uncomment the line below when you want to use it for input data to be sent to dashboard (buttons, switches, etc.)
+   *  led_f->onMessage(handleMessage); 
+   */
+
+  // wait for a connection
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
 }
 
 void loop() {
+    io.run();
 
 }
