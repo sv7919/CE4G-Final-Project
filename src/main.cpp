ef79407ae4811a1be2415440f111053a8a8d7881
sv7919 <vijayansiddarth@gmail.com>
2024-07-01 20:06:03 -0400

    comment: explains some conditionals and string

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,13 @@ void setup () {
 }
 
 void loop() {
+    /**
+     * @brief the while loop makes sure the GPS is fully initialized before running the rest of the code otherwise it never happens
+     */
     while (! GPS.fix) {
         GPSLoop();
     }
+
     GPSLoop();
     Serial.println();
     delay(5000);
@@ -29,11 +33,15 @@ void loop() {
     delay(5000);
 
     IOLoop();
+    /**
+     * @brief Simply lets the user know the dashboard has been updated through the serial monitor
+     */
     if (io.status() >= AIO_CONNECTED) {
         Serial.println("AdafruitIO dashboard updated");
     }
     delay(5000);
 
+    // figure out how to send the string to Adafruit IO
     String coordinates = String(String(lat) + c + String(lon) + c + String(ele) + c + String(tempreading) + c + String(humidreading) + c + String(pressreading));
     Serial.println(coordinates);
     Serial.println();
